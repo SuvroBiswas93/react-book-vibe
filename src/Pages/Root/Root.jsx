4a25b5e6226d198c6d02bcf9b8e8290191ace4f5
Suvro Biswas <shuvo.249963@gmail.com>
2025-10-04 21:55:30 +0600

    Inside root fixed navbar and footer component and add Outlet

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from '../../Components/Header/Navbar';
+import Footer from '../../Components/Footer/Footer';
+import { Outlet } from 'react-router';
 
 const Root = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
