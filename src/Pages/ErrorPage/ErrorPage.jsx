c8df571e26bf933e28d2cbbfa768ef67a26532b5
Suvro Biswas <shuvo.249963@gmail.com>
2025-10-04 22:20:55 +0600

    Add ErrorPage.jsx and set the element inside the route

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h3 className='text-2xl text-red-600'>Page Not found Error:404</h3>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router";
+import Root from '../Pages/Root/Root';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component:Root,
+    errorElement:<ErrorPage></ErrorPage>
   },
 ]);
